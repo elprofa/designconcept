c3c979a6fb795fcbbe36302375112915d11a11e7
KurtMoucketou <kurtmoucketou@yahoo.fr>
2021-05-11 09:50:04 +0000

    ModifDesign

---
+++ pages/teste.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import MetaData from '../components/Helmetcomponent'
+
+export default function Teste() {
+  return (
+    <div className="App">
+      <Head>
+        <title>Design | teste</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="main">
+        wedo
+      </main>
+
+    </div>
+  )
+}
